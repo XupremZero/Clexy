e564549149a776f689678459e68d073e53b164c3
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-11 11:25:48 +0100

    Add files via upload

---
+++ Types.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <omp.h>
+#define Temp template<class T>
+typedef void Void;
+typedef bool Bit;
+typedef unsigned char UByte;
+typedef signed char Byte;
+typedef unsigned short UShort;
+typedef signed short Short;
+typedef unsigned int UInt;
+typedef signed int Int;
+typedef unsigned long int UBig;
+typedef signed long int Big;
+typedef float Single;
+typedef double Double;
+typedef long double Quad;
+typedef UInt Size;
