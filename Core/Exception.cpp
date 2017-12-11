05f642a8a586d77a04bc8b36409d9fcda09e825a
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-11 14:59:06 +0100

    Add files via upload

---
+++ Core/Exception.cpp
@@ -0,0 +1,12 @@
+//Copyright 2018 Samuel Alonso, All Rights Reserved
+#include <Clexy\Core\Exception.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Void Clexy::Launch(const Severity s, const char* msg)
+{
+	FILE* fff = fopen(LogPath, "w");
+	fwrite(msg, 1, 300, fff);
+	fclose(fff);
+	abort();
+}
---
+++ Core/Exception.h
@@ -0,0 +1,9 @@
+//Copyright 2018 Samuel Alonso, All Rights Reserved
+#pragma once
+#include <Clexy\Types.h>
+#include <Clexy\Core\Debug.h>
+
+ns Clexy
+{
+	Void Launch(const Severity, const char*);
+}
