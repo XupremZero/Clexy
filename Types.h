cb88730dfb26f86f742e54b40bdb9c207c1fe45c
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-12 08:46:42 +0100

    Add files via upload

---
+++ Types.h
@@ -0,0 +1,21 @@
+//Copyright 2018 Samuel Alonso, All Rights Reserved
+#pragma once
+#include <omp.h>
+#include <stdint.h>
+#define Temp template<class T>
+#define ns namespace
+#define NoException
+typedef void Void;
+typedef bool Bit;
+typedef uint8_t UByte;
+typedef int8_t Byte;
+typedef uint16_t UShort;
+typedef int16_t Short;
+typedef uint32_t UInt;
+typedef int32_t Int;
+typedef uint64_t UBig;
+typedef int64_t Big;
+typedef float Single;
+typedef double Double;
+typedef long double Quad;
+typedef UInt Size;
