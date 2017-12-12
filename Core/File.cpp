1d8df750d79b2469fe424e2d733c23c65188edcb
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-12 06:24:38 +0100

    Add files via upload

---
+++ Core/File.cpp
@@ -0,0 +1,41 @@
+#include <Clexy\Core\File.h>
+#include <Clexy\Core\Exception.h>
+#include <stdio.h>
+
+Clexy::File::File(char* Direction)
+{
+	dir = Direction;
+	FILE* f = fopen(dir, "a+");
+	fclose(f);
+}
+
+Byte* Clexy::File::Read()
+{
+	FILE* f = fopen(dir, "r");
+	fseek(f, 0, SEEK_END);
+	Int s = ftell(f);
+	rewind(f);
+	Byte* buffer;
+	fread(buffer, 1, s, f);
+	fclose(f);
+	return buffer;
+}
+
+Void Clexy::File::Write(const char* Content)
+{
+	FILE* f = fopen(dir, "w");
+	fwrite(Content, 1, sizeof(Content), f);
+	fclose(f);
+}
+
+Void Clexy::File::Rename(char* NDirection)
+{
+	rename(dir, NDirection);
+	dir = NDirection;
+}
+
+Void Clexy::File::Remove()
+{
+	remove(dir);
+	dir = nullptr;
+}
