0ec41d0106146c9251aed446184453ab22bd179f
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-14 11:27:01 +0100

    Delete Exception.h

--- Core/Exception.h
+++
@@ -1,9 +0,0 @@
-//Copyright 2018 Samuel Alonso, All Rights Reserved
-#pragma once
-#include <Clexy\Types.h>
-
-ns Clexy
-{
-	//Write an exception info onto a log file based on the argument.
-	Void Launch(const Byte*);
-}
