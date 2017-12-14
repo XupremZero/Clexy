e641cb7c797678afcba54a41a1b7e147a588e189
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-14 11:32:00 +0100

    Add files via upload

--- Core/Debug.h
+++ Core/Debug.h
@@ -1,17 +1,13 @@
 //Copyright 2018 Samuel Alonso, All Rights Reserved
 #pragma once
+#include <Clexy\Types.h>
 
-enum Severity : unsigned char
+enum Severity : UByte
 {
 	Info, Warn, Error
 };
 
-#ifdef ClexyDebug
-#include <Clexy\Types.h>
 /*Write a debug message based on the second argument. If Severity level is equal to Info the program will continue executing,
 if it's equal to Warn, the program will continue to execute or not depending if the third argument is true or false(true to continue, false to exit).
 Finally if the Severity level is equal to Error, the program will abort the process.*/
 Void Log(const Severity, const Byte*, const Bit);
-#else
-#define Log(x, y, z)
-#endif
